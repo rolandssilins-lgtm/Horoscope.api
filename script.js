af64ae658055c8ade5596e940c0a626cf982e5db
marta-te <122258127+marta-te@users.noreply.github.com>
2025-11-27 11:10:13 +0200

    comment about testing

--- script.js
+++ script.js
@@ -34,6 +34,7 @@ async function getHoroscope() {
   const proxyUrl = "https://corsproxy.io/?";
   const url = proxyUrl + encodeURIComponent(originalUrl);
 
+  // for testing purposes with live server change originalUrl with url
   try {
     const response = await fetch(originalUrl);
 
